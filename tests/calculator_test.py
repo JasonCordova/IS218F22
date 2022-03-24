253ff97c92951a6ca0132bbaa7f0cc94a20d114c
Jason Cordova <jasoncordova.edu@gmail.com>
2022-03-24 10:41:27 -0400

    Trying to pull from repo

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -6,7 +6,7 @@ from calculator import Calculator
 def test_calculator_add_method():
     """Testing the Calculator"""
     # this is show using the calculator object add method
-    tuple_list = (1,2)
+    tuple_list = (1.0,2)
     assert Calculator.add(tuple_list) == 3
 
 def test_calculator_subtract_method():
