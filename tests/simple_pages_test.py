a720f95a829b0cac286a08f7689a9f58c46d98a8
JasonCordova <65292469+JasonCordova@users.noreply.github.com>
2022-04-30 00:21:33 -0400

    Revert "Development to Production"

--- tests/simple_pages_test.py
+++ tests/simple_pages_test.py
@@ -4,28 +4,68 @@ def test_request_main_menu_links(client):
     """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
-    assert b'href="/about"' in response.data
-    assert b'href="/welcome"' in response.data
-    assert b'href="/login"' in response.data
-    assert b'href="/register"' in response.data
-
+    assert b'<a class="nav-link" href="/git">Git</a>' in response.data
+    assert b'<a class="nav-link" href="/docker">Docker</a>' in response.data
+    assert b'<a class="nav-link" href="/pythonflask">Python/Flask</a>' in response.data
+    assert b'<a class="nav-link" href="/continuous">Continuous Integration/Deployment</a>' in response.data
 
 def test_request_index(client):
-    """This gets the index page"""
+    """This makes the index page"""
     response = client.get("/")
     assert response.status_code == 200
+    assert b"Index Page" in response.data
 
-def test_request_about(client):
-    """This gets the about page"""
-    response = client.get("/about")
+def test_request_gitpage(client):
+    """This makes the index page"""
+    response = client.get("/git")
     assert response.status_code == 200
+    assert b"git" in response.data
 
-def test_request_page1(client):
-    """This gets the welcome page"""
-    response = client.get("/welcome")
+def test_request_dockerpage(client):
+    """This makes the index page"""
+    response = client.get("/docker")
     assert response.status_code == 200
+    assert b"docker" in response.data
 
-def test_request_page_not_found(client):
-    """This gets a page that does not exist"""
-    response = client.get("/page5")
-    assert response.status_code == 404
+def test_request_pythonflask(client):
+    """This makes the index page"""
+    response = client.get("/pythonflask")
+    assert response.status_code == 200
+    assert b"pythonflask" in response.data
+
+def test_request_continuous(client):
+    """This makes the index page"""
+    response = client.get("/continuous")
+    assert response.status_code == 200
+    assert b"continuous" in response.data
+
+# ----------------------------------------------------
+
+def test_request_article1(client):
+    """This makes the index page"""
+    response = client.get("/article1")
+    assert response.status_code == 200
+    assert b"Pylint" in response.data
+
+def test_request_article2(client):
+    """This makes the index page"""
+    response = client.get("/article2")
+    assert response.status_code == 200
+    assert b"AAA Testing" in response.data
+
+def test_request_article3(client):
+    """This makes the index page"""
+    response = client.get("/article3")
+    assert response.status_code == 200
+    assert b"Object-oriented programming" in response.data
+
+def test_request_article4(client):
+    """This makes the index page"""
+    response = client.get("/article4")
+    assert response.status_code == 200
+    assert b"Overview of SOLID" in response.data
+
+#def test_request_page_not_found(client):
+ #   """This makes the index page"""
+  #  response = client.get("/page5")
+   # assert response.status_code == 404
