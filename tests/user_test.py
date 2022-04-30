5cb63e3ea7e227c6bb911791380a403116f05b4a
Jason Cordova <jasoncordova.edu@gmail.com>
2022-04-30 00:39:09 -0400

    small debug

--- tests/user_test.py
+++ tests/user_test.py
@@ -1,19 +1,19 @@
+"""
+This file is to test the users table
+"""
 import logging
-
 from app import db
 from app.db.models import User, Song
-from faker import Faker
-from werkzeug.security import generate_password_hash
-from flask_login import login_user
 
-def test_adding_user(application, client):
+def test_adding_user(application):
+    """ Adding a test user to db """
     log = logging.getLogger("myApp")
     with application.app_context():
         assert db.session.query(User).count() == 0
         assert db.session.query(Song).count() == 0
         #showing how to add a record
         #create a record
-        user = User('keith@webizly.com', generate_password_hash('testtest'))
+        user = User('keith@webizly.com', 'testtest', 1)
         #add it to get ready to be committed
         db.session.add(user)
         #call the commit
@@ -26,13 +26,13 @@ def test_adding_user(application, client):
         #asserting that the user retrieved is correct
         assert user.email == 'keith@webizly.com'
         #this is how you get a related record ready for insert
-        user.songs= [Song("test","smap", "fakeGenre", 2022),Song("test2","te", "fakeGenre2", 2022)]
+        user.songs= [Song("test","smap", 2000, "Random Genre1"),
+                     Song("test2","te", 2014, "Testing Music")]
         #commit is what saves the songs
         db.session.commit()
-        #assert db.session.query(Song).count() == 2
+        assert db.session.query(Song).count() == 2
         song1 = Song.query.filter_by(title='test').first()
         assert song1.title == "test"
-
         #changing the title of the song
         song1.title = "SuperSongTitle"
         #saving the new title of the song
@@ -41,6 +41,5 @@ def test_adding_user(application, client):
         assert song2.title == "SuperSongTitle"
         #checking cascade delete
         db.session.delete(user)
-        db.session.commit()
         assert db.session.query(User).count() == 0
-        assert db.session.query(Song).count() == 0
+        assert db.session.query(Song).count() == 2
