9e7bebfd0d85c00b3ffa70048c952b512c7d1600
Jason Cordova <jasoncordova.edu@gmail.com>
2022-05-11 12:32:59 -0400

    small change

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -11,7 +11,6 @@ def test_request_main_menu_links(client):
     assert b'href="/login"' in response.data
     assert b'href="/register"' in response.data
 
-
 def test_auth_pages(client):
     """This makes the index page"""
     response = client.get("/dashboard")
